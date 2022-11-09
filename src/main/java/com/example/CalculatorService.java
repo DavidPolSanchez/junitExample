74f190805ad684692c1445c84c770b5a271a85d7
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2022-11-09 16:14:32 +0100

    Initial commit

---
+++ src/main/java/com/example/CalculatorService.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class CalculatorService {
+
+    public int sum(int num1, int num2){
+        if(num1 < 0 || num2 < 0) {
+            return 0;
+        }
+        return num1 + num2;
+    }
+
+}
